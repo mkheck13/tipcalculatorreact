5209111886c35f645b49b076e65a42742f5ddb0c
mkheck13 <mkheck13@gmail.com>
2025-03-12 15:23:03 -0700

    working on responsiveness

--- src/components/CalculatorComponent.jsx
+++ src/components/CalculatorComponent.jsx
@@ -12,18 +12,37 @@ const CalculatorComponent = () => {
     const [tipPer, setTipPer] = useState(0);
     const [totalPer, setTotalPer] = useState(0);
 
+    // useEffect(() => {
+    //     if (bill > 0 && people > 0 && tip > 0) {
+    //         setTipPer((bill * tip) / people);
+    //         setTotalPer((bill + bill * tip) / people);
+    //     }
+    // }, [tip, bill, people]);
+
     useEffect(() => {
         if (bill > 0 && people > 0 && tip > 0) {
-            setTipPer((bill * tip) / people);
-            setTotalPer((bill + bill * tip) / people);
+            const tipAmount = (bill * tip) / people;
+            const totalAmount = (bill + bill * tip) / people;
+            setTipPer(tipAmount);
+            setTotalPer(totalAmount);
         }
     }, [tip, bill, people]);
+
+    const handleTipSelection = (percentage) => {
+        setTip(percentage / 100);
+        setSelectedButton(percentage);
+    }
+
+    const handleInputChange = (setter) => (e) => {
+        const value = parseFloat(e.target.value);
+        setter(isNaN(value) ? 0 : value);
+    }
 
     const handleReset = () => {
         setBill(0);
         setPeople(0);
         setTip(0);
-        setSelectedButton('');
+        setSelectedButton(null);
         setTipPer(0);
         setTotalPer(0);
     };
@@ -31,7 +50,7 @@ const CalculatorComponent = () => {
   return (
     <div className='flex min-h-screen flex-col items-center pt-10 md:p-24'>
             <img src={logo} alt='Splitter Logo' className='mx-auto' />
-            <div className='bg-white w-screen md:w-7/12 h-1/3 shadow-sm shadow-grayCyan rounded-2xl md:mx-auto mt-20 md:flex px-5 py-4 md:p-2 max-md:text-sm'>
+            <div className='flex flex-col lg:flex-row bg-white w-screen md:w-7/12 h-1/3 shadow-sm shadow-grayCyan rounded-2xl md:mx-auto mt-20 px-5 py-4 md:p-2 max-md:text-sm'>
                 <div className='w-full md:w-1/2'>
                     <div className='md:pl-10 md:pr-3 py-6'>
                         <p className='text-darkGrayCyan'>Bill</p>
@@ -39,35 +58,54 @@ const CalculatorComponent = () => {
                             <span className='absolute text-gray-400 text-2xl font-normal top-[14px] left-4'>
                                 <img src={dollar} alt='Dollar Sign' />
                             </span>
-                            <input value={bill === 0 ? '' : bill} type='number' placeholder='0' className='bg-lightGray h-12 rounded-xl text-xl text-right pr-5 w-full' onChange={(e) => setBill(parseFloat(e.target.value))} min={0} onKeyDown={(evt) => ['e', 'E', '+', '-'].includes(evt.key) && evt.preventDefault()} />
+                            <input 
+                            value={bill || ''} 
+                            type='number' 
+                            placeholder='0' 
+                            className='bg-lightGray h-12 rounded-xl text-xl text-right pr-5 w-full' 
+                            onChange={handleInputChange(setBill)} 
+                            min={0} 
+                            onKeyDown={(evt) => ['e', 'E', '+', '-'].includes(evt.key) && evt.preventDefault()} />
                         </div>
                     </div>
 
                     <div className='md:pl-10'>
                         <p className="pt-10 pb-3">Select Tip %</p>
-                        <div className='grid grid-cols-2 md:grid-cols-3 gap-3 text-xl sm:text-lg'>
-                            {[5, 10, 15, 25, 50].map((percentage, index) => (
-                                <button key={index} className={`${selectedButton === index ? 'bg-strongCyan text-veryDarkCyan' : 'bg-veryDarkCyan text-white'} text-2xl w-full md:w-28 h-12 rounded-lg font-semibold hover:bg-lightGrayCyan hover:text-veryDarkCyan`} onClick={() => {
-                                    setSelectedButton(index);
-                                    setTip(percentage / 100);
-                                }}>
+                        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-4 text-xl sm:text-lg'>
+                            {[5, 10, 15, 25, 50].map((percentage) => (
+                                <button key={percentage} 
+                                className={`${selectedButton === percentage ? 'bg-strongCyan text-veryDarkCyan' : 'bg-veryDarkCyan text-white'} text-2xl w-full min-w-[80px] h-12 rounded-lg font-semibold hover:bg-lightGrayCyan hover:text-veryDarkCyan`} 
+                                onClick={() => handleTipSelection(percentage)}>
                                     {percentage}%
                                 </button>
                             ))}
-                            <input className='text-2xl bg-lightGray w-11/12 md:w-28 h-12 rounded-lg font-semibold text-veryDarkCyan text-right pr-5 md:pr-3' type='text' placeholder='Custom' onClick={() => setSelectedButton('')} onChange={(e) => setTip(parseFloat(e.target.value) / 100)} />
+                            <input 
+                            className="text-2xl bg-lightGray w-full min-w-[80px] h-12 rounded-lg font-semibold text-veryDarkCyan text-right pr-5 md:pr-3" 
+                            type='number' 
+                            placeholder='Custom' 
+                            value={tip > 0 ? tip * 100 : ''}
+                            onClick={() => setSelectedButton(null)} 
+                            onChange={(e) => setTip(e.target.value ? parseFloat(e.target.value) / 100 : 0)} />
                         </div>
                     </div>
 
                     <div className='md:pl-10 pr-3 py-6'>
                         <div className='flex justify-between'>
                             <p className='text-darkGrayCyan'>Number of People</p>
-                            <p className='text-orange-500 pr-2'>{people === 0 ? "Can't be zero" : ''}</p>
+                            <p className='text-orange-500 pr-2'>{people === 0 && bill > 0 ? "Can't be zero" : ''}</p>
                         </div>
                         <div className='relative'>
                             <span className='absolute text-gray-400 text-2xl font-normal top-[14px] left-4'>
                                 <img src={person} alt='Person Icon' />
                             </span>
-                            <input value={people === 0 ? '' : people} type='number' placeholder='0' className='bg-lightGray h-12 rounded-xl text-xl text-right pr-5 w-full' onChange={(e) => setPeople(parseFloat(e.target.value))} min={0} onKeyDown={(evt) => ['e', 'E', '+', '-'].includes(evt.key) && evt.preventDefault()} />
+                            <input 
+                            value={people || ''} 
+                            type='number' 
+                            placeholder='0' 
+                            className='bg-lightGray h-12 rounded-xl text-xl text-right pr-5 w-full' 
+                            onChange={handleInputChange(setPeople)} 
+                            min={1} 
+                            onKeyDown={(evt) => ['e', 'E', '+', '-'].includes(evt.key) && evt.preventDefault()} />
                         </div>
                     </div>
                 </div>
@@ -78,14 +116,14 @@ const CalculatorComponent = () => {
                                 <p className='text-white'>Tip Amount</p>
                                 <p className='text-lightGrayCyan opacity-50 text-sm'>/ person</p>
                             </div>
-                            <p className='text-strongCyan text-2xl md:text-4xl'>{`$${tipPer.toFixed(2)}`}</p>
+                            <p className='text-strongCyan text-2xl md:text-4xl'>{tipPer > 0 ? `$${tipPer.toFixed(2)}` : '$0.00'}</p>
                         </div>
                         <div className='flex px-10 py-4 md:p-10 justify-between'>
                             <div>
                                 <p className='text-white'>Total</p>
                                 <p className='text-lightGrayCyan opacity-50 text-sm'>/ person</p>
                             </div>
-                            <p className='text-strongCyan text-2xl md:text-4xl'>{`$${totalPer.toFixed(2)}`}</p>
+                            <p className='text-strongCyan text-2xl md:text-4xl'>{totalPer > 0 ? `$${totalPer.toFixed(2)}` : '$0.00'}</p>
                         </div>
                     </div>
                     <div className='px-4 md:px-10 pb-4 pt-3 md:py-6 mt-0 md:mt-20'>
